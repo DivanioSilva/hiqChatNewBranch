7d33b08c236ae56682ad12581f7e33ae6b830eaa
João Gomes <jgomes@starbuck.local>
2018-03-01 20:50:56 +0000

    novo codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthService } from './shared/auth.service';
+import { LoginComponent } from './auth/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class AuthService {
 
-    public userState: boolean;
+    public userState: boolean = false;
     
     public login(): void{
         this.userState = true;
