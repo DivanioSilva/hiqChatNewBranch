2423da8a020ea05744ca93caa64d96a7b5c3d44e
João Gomes <jgomes@starbuck.local>
2018-03-06 20:14:33 +0000

    firebase code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { AuthService } from './shared/auth.service';
 import { LoginComponent } from './auth/login.component';
@@ -15,7 +22,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.config),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiKey: "AIzaSyBikClwAiMOahxFG7annnRiNFMWM-enlVw",
+    authDomain: "hiqchat-a69bb.firebaseapp.com",
+    databaseURL: "https://hiqchat-a69bb.firebaseio.com",
+    projectId: "hiqchat-a69bb",
+    storageBucket: "hiqchat-a69bb.appspot.com",
+    messagingSenderId: "532430869685"
+  }
 };
